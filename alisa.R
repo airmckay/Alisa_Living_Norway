6b46fb883511e5e135628f10d85c83d4a6d88538
airmckay <apmc@nmbu.no>
2022-11-17 10:16:18 +0100

    catch up

---
+++ alisa.R
@@ -0,0 +1,19 @@
+# Follow this tutorial 
+# https://biostats-r.github.io/biostats/github/1_Git_Tutorial.html
+
+###############################
+## Set up git hub connection ##
+
+library(usethis)
+use_github() 
+git_default_branch_rename() 
+
+git_default_branch_configure(name = "main")
+
+###############################
+## load packages ##
+
+library(tidyverse)
+
+# Create a gitignore text file 
+# sharing a repository is actually one of the worst ways to collaborate with fellow users
